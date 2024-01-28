07a8403ef32b460590c481704f1bf536dde86d73
Oleksandr Nykytin <keeper@ninydev.com>
2024-01-28 15:03:10 +0200

    Java :: Collections Type

--- Lesson1_maven/src/main/java/org/example/works/jan28/CollectionStart.java
+++ Lesson1_maven/src/main/java/org/example/works/jan28/CollectionStart.java
@@ -3,6 +3,8 @@ package org.example.works.jan28;
 import org.example.entities.baggage.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class CollectionStart implements Runnable
 {
@@ -11,7 +13,110 @@ public class CollectionStart implements Runnable
         asInterface();
     }
 
+    /**
+     * Коллекция - как коробка - должна обладать набором методов работы с ней
+     * - положить в коробку
+     * - достать из коробки
+     * - сколько в коробке элементов
+     */
+    private void simpleCollectionAsInterface() {
+        Collection<BaggageInterface> simpleCollection = new Collection<BaggageInterface>() {
 
+            private ArrayList<BaggageInterface> data = new ArrayList<>();
+
+            // Сколько элементов в коробке
+            @Override
+            public int size() {
+                return data.size();
+                // return 0;
+            }
+
+            // Сообщать что коробка пуста
+            @Override
+            public boolean isEmpty() {
+                return  data.isEmpty();
+                // return false;
+            }
+
+            // Есть ли такой элемент в коллекции
+            @Override
+            public boolean contains(Object o) {
+                return data.contains(o);
+            }
+
+            // Указалель на очередной элемент - нужен для перебора
+            @Override
+            public Iterator<BaggageInterface> iterator() {
+                return data.iterator();
+            }
+
+            // Используется для экспорта коллекции в массив Object
+            @Override
+            public Object[] toArray() {
+                return data.toArray();
+            }
+
+            // Для экспорта в определенный тип (если такое возможно
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            // Положить в коллекцию
+            @Override
+            public boolean add(BaggageInterface element) {
+                return data.add(element);
+            }
+
+            // Убрать из коллекции
+            @Override
+            public boolean remove(Object o) {
+                return data.remove(o);
+            }
+
+            // Сравнить коллекции
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return data.containsAll(c);
+            }
+
+            // Добавить элементы из другой коллекции
+            @Override
+            public boolean addAll(Collection<? extends BaggageInterface> c) {
+                return data.addAll(c);
+            }
+
+            // Убрать элементы
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return data.removeAll(c);
+            }
+
+            // Удалить лишние
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return data.retainAll(c);
+            }
+
+            // Очистить коллекцию
+            @Override
+            public void clear() {
+                data.clear();
+            }
+
+            // Срквить коробку
+            @Override
+            public boolean equals(Object o) {
+                return false;
+            }
+
+            // Вернуть хеш класса коробки
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+        };
+    }
 
     private void asInterface(){
         /**
