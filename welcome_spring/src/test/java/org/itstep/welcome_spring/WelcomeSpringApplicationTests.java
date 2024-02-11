b099c75166eb1e7fab9a9044e081e94593c094c1
Oleksandr Nykytin <keeper@ninydev.com>
2024-02-11 14:58:53 +0200

    Spring Start

---
+++ welcome_spring/src/test/java/org/itstep/welcome_spring/WelcomeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itstep.welcome_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
